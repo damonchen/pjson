d219176228d9b779da70117a2feb4eca6d9a5625
tianqi <netubu@gmail.com>
2024-05-25 18:48:17 +0800

    change parser function due to understand monad

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # pjson
+
 using goparsec2 to parse json
 
 ## depend
@@ -6,3 +7,12 @@ using goparsec2 to parse json
 `goparsec2`
 
     go get github.com/Dwarfartisan/goparsec2
+
+
+## understand parsec
+
+使用parsec之前，需要先理解monad，理解monad，可以这么考虑
+
+1. 有一个算子，`parsec`处为 `P`，经过一系列操作后，返回的结果依旧是 `P`
+2. 这个算子在运算过程中，会先将`P`解开来，得到里面的值，然后封装为`P`
+
--- parse.go
+++ parse.go
@@ -1,30 +1,13 @@
 package pjson
 
-import p "github.com/Dwarfartisan/goparsec2"
+import (
+	p "github.com/Dwarfartisan/goparsec2"
+)
 
 // Parse 分析JSON源码
 func Parse(code string) (interface{}, error) {
 	st := p.BasicStateFromText(code)
-	var v interface{}
-	var e error
-
-	for {
-		Skip(&st)
-		_, err := p.Try(p.EOF)(&st)
-		if err == nil {
-			break
-		}
-
-		value, err := ValueParser()(&st)
-		if err != nil {
-			return nil, err
-		}
-
-		switch vv := value.(type) {
-		default:
-			v = vv
-			e = nil
-		}
-	}
-	return v, e
+	// yes, i understand the monad
+	parser := p.Try(Skip).Then(ValueParser())
+	return parser.Parse(&st)
 }
